e5da75849d32a34632f4683b576631835e134a32
tahdi-islam <tshdi60@gmail.com>
2022-09-29 21:06:07 +0600

    edit in exercise compo

--- src/Component/All-Exercise/AllExercise.js
+++ src/Component/All-Exercise/AllExercise.js
@@ -8,10 +8,10 @@ const AllExercise = ({exercise, setDuration}) => {
     return (
       <div className="exercise-container relative bg-blue-100 p-2 rounded-lg flex flex-col sm:items-center">
         <img src={img} className="h-72  rounded-md" alt="" />
-        <div>
-          <h3 className="text-2xl font-medium">{name}</h3>
-          <p>For Age: {age}</p>
-          <p>Time Required: {duration}s</p>
+        <div className='w-full'>
+          <h3 className="text-2xl font-semibold">{name}</h3>
+          <p className="font-medium">For Age: {age}</p>
+          <p className="font-medium">Time Required: {duration}s</p>
         </div>
         <button
           onClick={() => setDuration((previous) => previous + duration)}
