ed6c48a4567c419b8be3df5a9ba1dc42dbd292a5
tahdi-islam <tshdi60@gmail.com>
2022-09-29 11:39:09 +0600

    add use details

--- src/Component/Side-Bar/SideBar.js
+++ src/Component/Side-Bar/SideBar.js
@@ -9,6 +9,20 @@ const SideBar = () => {
             <img className="w-10 rounded-lg" src={userImg} alt="user-images" />
             <h3 className="text-xl font-bold ml-3">Tahdi Islam</h3>
           </div>
+          <div className="flex justify-around my-4 bg-blue-100 px-3 py-2 rounded-lg">
+            <div className="flex flex-col items-center">
+              <h2 className="text-xl font-semibold">75kg</h2>
+              <h3 className="text-xl font-medium">Weight</h3>
+            </div>
+            <div className="flex flex-col items-center">
+              <h2 className="text-xl font-semibold">75kg</h2>
+              <h3 className="text-xl font-medium">Weight</h3>
+            </div>
+            <div className="flex flex-col items-center">
+              <h2 className="text-xl font-semibold">75kg</h2>
+              <h3 className="text-xl font-medium">Weight</h3>
+            </div>
+          </div>
         </div>
         <div className="user-activity"></div>
       </div>
