fc9cc489f1bf031152f15cabf67a98013f056a9e
tahdi-islam <tshdi60@gmail.com>
2022-09-29 07:52:09 +0600

    install tailwind css

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className='text-green-500'>
+            <h1>TI Fitness Club</h1>
+        </header>
+    );
+};
+
+export default Header;
