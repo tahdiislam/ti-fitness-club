879a549e23a3e60c2210dfe3d1810a5f0faafc5a
tahdi-islam <tshdi60@gmail.com>
2022-09-29 12:38:28 +0600

    edit json

--- src/Component/All-Exercise/AllExercise.js
+++ src/Component/All-Exercise/AllExercise.js
@@ -5,14 +5,14 @@ const AllExercise = ({exercise}) => {
     console.log(exercise)
     const {img, name, age, duration} = exercise
     return (
-      <div className='exercise-container relative'>
+      <div className='exercise-container relative bg-blue-100 p-2 rounded-lg'>
         <img src={img} className='h-72 rounded-md' alt="" />
         <div>
           <h3 className='text-2xl font-medium'>{name}</h3>
           <p>For Age: {age}</p>
-          <p>Time Required: {duration}</p>
+          <p>Time Required: {duration}s</p>
         </div>
-        <button className='bg-blue-600 hover:bg-blue-800 text-white hover:text-gray-300 px-8 py-3 w-full absolute bottom-0 rounded-lg'>Add To List</button>
+        <button className='bg-blue-600 hover:bg-blue-800 text-white hover:text-gray-300 px-8 py-3 w-full absolute bottom-0 left-0 rounded-lg'>Add To List</button>
       </div>
     );
 };
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,7 +5,7 @@ import logo from '../../images/logo.png'
 const Header = () => {
     return (
       <header>
-        <a href="#home" className="text-blue-500 text-4xl font-semibold flex mt-6">
+        <a href="#home" className="text-blue-600 text-4xl font-semibold flex mt-6">
           <img className="mr-2" src={logo} alt="logo" />
           <h1>TI Fitness Club</h1>
         </a>
